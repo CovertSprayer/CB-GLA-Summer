515bacc87aeef3d185a317160dd7937388dc960a
Shreyance Bodh <shreyancebodh2013@gmail.com>
2023-07-14 13:45:26 +0530

    Lec 18

---
+++ Lec-18/sortNumber/index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+function getData(fileName) {
+
+    return new Promise((resolve, reject) => {
+        const F1 = path.join(__dirname, 'data', fileName);
+        fs.readFile(F1, 'utf-8', (err, data) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
+        });
+    })
+}
+
+// getData('input1.txt')
+//     .then((data1)=>{
+//         const arr1 = data1.split('\r\n');
+//         getData('input2.txt')
+//             .then((data2)=>{
+//                 const arr2 = data2.split('\r\n');
+//                 const arr = [...arr1, ...arr2];
+//                 arr.sort((a,b)=> {
+//                     return a-b;
+//                 })
+
+//                 const F3 = path.join(__dirname, 'data', 'output.txt');
+//                 const finalData = arr.join('\n');
+
+//                 fs.writeFile(F3, finalData, (err)=>{
+//                     if(err){
+//                         console.log(err);
+//                         return;
+//                     }
+//                     console.log('File Written Successfully!!');
+//                 })
+
+//             })
+//     })
+
+
+
+
+// fs.readFile(F2, 'utf-8', (err, data) => {
+//     input2 = data;
+// });
+
+// console.log(input1);
+// console.log(input2);
+
+// let input = [...input1, ...input2];
+
+// console.log(input);
+
+
+
+
+
+
